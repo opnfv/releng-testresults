158bc23c00484150487d3601722b7580b58481d9
thuva4 <tharma.thuva@gmail.com>
2018-02-08 23:01:16 +0530

    add testcase in testapi client
    implement CRUD operations for testcases in
    testapi client
    
    Change-Id: I84cc8112be10469c3da1f4604507cbc021310e4f
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

---
+++ testapi/testapi-client/testapiclient/testcase.py
@@ -0,0 +1,68 @@
+import json
+from user import User
+from cliff.command import Command
+from httpClient import HTTPClient
+from config import Config
+
+
+class TestCaseBase(Command):
+    projects_url = Config.config.get("api", "url") + "/projects/"
+
+
+class TestCaseGet(TestCaseBase):
+
+    def get_parser(self, prog_name):
+        parser = super(TestCaseGet, self).get_parser(prog_name)
+        parser.add_argument('-project', type=str, required=True, help='Get all testcases for a project name')
+        return parser
+
+    def take_action(self, parsed_args):
+        httpClient = HTTPClient.get_Instance()
+        TestCases = httpClient.get(TestCaseGet.projects_url + parsed_args.project + "/cases")
+        print TestCases
+
+
+class TestCaseCreate(TestCaseBase):
+
+    def get_parser(self, prog_name):
+        parser = super(TestCaseCreate, self).get_parser(prog_name)
+        parser.add_argument('-project', type=str, required=True, help='Create testcase under a project name')
+        parser.add_argument('-case', type=json.loads, required=True, help='Testcase create request format:\n{"run": "", "name": "", "ci_loop": "", "tags": "",\n "url": "", "blocking": "", "domains": "", "dependencies": "",\n "version": "", "criteria": "", "tier": "", "trust": "",\n "catalog_description": "", "description": ""}')
+        return parser
+
+    def take_action(self, parsed_args):
+        httpClient = HTTPClient.get_Instance()
+        response = httpClient.post(TestCaseCreate.projects_url + parsed_args.project + "/cases", User.session, parsed_args.case)
+        if response.status_code == 200:
+            print "TestCase has been successfully created!"
+        else:
+            print response.text
+
+
+class TestCaseDelete(TestCaseBase):
+
+    def get_parser(self, prog_name):
+        parser = super(TestCaseDelete, self).get_parser(prog_name)
+        parser.add_argument('-project', type=str, required=True, help='Delete testcase under project')
+        parser.add_argument('-name', type=str, required=True, help='Delete testcase by name')
+        return parser
+
+    def take_action(self, parsed_args):
+        httpClient = HTTPClient.get_Instance()
+        TestCases = httpClient.delete(TestCaseDelete.projects_url + parsed_args.project + "/cases/" + parsed_args.name, User.session)
+        print TestCases
+
+
+class TestCasePut(TestCaseBase):
+
+    def get_parser(self, prog_name):
+        parser = super(TestCasePut, self).get_parser(prog_name)
+        parser.add_argument('-project', type=str, required=True, help='Update the testcase under project')
+        parser.add_argument('-name', type=str, required=True, help='Update the testcase by name')
+        parser.add_argument('-case', type=json.loads, required=True, help='Testcase update request format:\n{"run": "", "name": "", "ci_loop": "", "tags": "",\n "url": "", "blocking": "", "domains": "", "dependencies": "",\n "version": "", "criteria": "", "tier": "", "trust": "",\n "catalog_description": "", "description": ""}')
+        return parser
+
+    def take_action(self, parsed_args):
+        httpClient = HTTPClient.get_Instance()
+        TestCases = httpClient.put(TestCasePut.projects_url + parsed_args.project + "/cases/" + parsed_args.name, User.session, parsed_args.case)
+        print TestCases
