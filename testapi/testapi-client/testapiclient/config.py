ee6b34f6eb112ccceb8eaae6376db41575fbf3e0
thuva4 <tharma.thuva@gmail.com>
2018-02-25 17:17:52 +0530

    Add tests for Pods
    Add tests for pods in testapiclient
    
    Change-Id: Ifc7962802a81b82a7a298243cb719c20049f65d9
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

--- testapi/testapi-client/testapiclient/config.py
+++ testapi/testapi-client/testapiclient/config.py
@@ -4,11 +4,21 @@ import io
 
 class Config:
     # Load the configuration file
-    config = ''
+    config = {}
 
     @staticmethod
     def parse_conf():
-        with open("/etc/testapiclient/config.ini") as f:
-            sample_config = f.read()
+        try:
+            with open("/etc/testapiclient/config.ini") as f:
+                sample_config = f.read()
+        except IOError:
+            with open("../etc/config.ini") as f:
+                sample_config = f.read()
         Config.config = ConfigParser.RawConfigParser(allow_no_value=True)
         Config.config.readfp(io.BytesIO(sample_config))
+
+    @staticmethod
+    def get_conf():
+        if type(Config.config) != "instance":
+            Config.parse_conf()
+        return Config.config
