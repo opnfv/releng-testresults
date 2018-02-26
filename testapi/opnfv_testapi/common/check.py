0ec2df42d31cfeeecc2bad67cef45cbac8b0cbe2
thuva4 <tharma.thuva@gmail.com>
2018-02-26 09:48:45 +0530

    Prevent to delete the pods
    Do not allow to delete pods which are associated
    with test-results
    
    JIRA: RELENG-350
    
    Change-Id: I8cb306d719acebff257048f08bcb981d81c64513
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

--- testapi/opnfv_testapi/common/check.py
+++ testapi/opnfv_testapi/common/check.py
@@ -35,7 +35,7 @@ def is_authorized(method):
                 if type(query) is not dict:
                     query_data = query()
                 else:
-                    if self.json_args is None:
+                    if self.json_args is None or 'name' not in self.json_args:
                         query_data = query
                     else:
                         query_data = self.json_args
@@ -47,15 +47,22 @@ def is_authorized(method):
     return wrapper
 
 
-def is_allowed(method):
+def is_reource_tied(method):
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        if self.table == 'projects':
-            query_data = {}
-            query_data['project_name'] = kwargs.get('query')['name']
-            data = yield dbapi.db_find_one('testcases', query_data)
-            if data:
-                raises.Unauthorized(message.tied_with_resource())
+        query_data = {}
+        tied_map_tables = {'projects': ['testcases'], 'pods': ['results'], 'testcases': ['results']}
+        query_naming = {'testcases': 'case_name', 'projects': 'project_name', 'pods': 'pod_name'}
+        if self.table in ['projects', 'pods', 'testcases']:
+            if method.__name__ == '_update':
+                if 'name' not in self.json_args:
+                    ret = yield gen.coroutine(method)(self, *args, **kwargs)
+                    raise gen.Return(ret)
+            query_data[query_naming[self.table]] = kwargs.get('query')['name']
+            for table in tied_map_tables[self.table]:
+                data = yield dbapi.db_find_one(table, query_data)
+                if data:
+                    raises.Unauthorized(message.tied_with_resource())
         ret = yield gen.coroutine(method)(self, *args, **kwargs)
         raise gen.Return(ret)
     return wrapper
