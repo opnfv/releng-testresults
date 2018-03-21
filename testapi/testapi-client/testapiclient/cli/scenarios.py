76f3a790d042ea1061728d26cdd16d6b5c3ac2e6
thuva4 <tharma.thuva@gmail.com>
2018-03-21 15:46:30 +0530

    Add installers CRUD in testapiclient
    implement interface to do CRUD operations
    for installers in testapiclient
    Tests are added
    
    Change-Id: I025b01c5b48d5b04cfcead8ee7818c0a1752cec0
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

--- testapi/testapi-client/testapiclient/cli/scenarios.py
+++ testapi/testapi-client/testapiclient/cli/scenarios.py
@@ -12,6 +12,15 @@ def scenario_url(parsed_args):
     return urlparse.path_join(scenarios_url(), parsed_args.name)
 
 
+def installers_url(name):
+    return urlparse.resource_join('scenarios', name, 'installers')
+
+
+def installer_url(parsed_args):
+    return urlparse.path_join(
+        installers_url(parsed_args.scenario_name), parsed_args.name)
+
+
 class ScenarioGet(command.Lister):
 
     def get_parser(self, prog_name):
@@ -129,3 +138,59 @@ class ScenarioPut(command.ShowOne):
         return self.format_output(
             self.app.client_manager.put(
                 scenario_url(parsed_args), parsed_args.scenario))
+
+
+class InstallerCreate(command.Command):
+
+    def get_parser(self, prog_name):
+        parser = super(InstallerCreate, self).get_parser(prog_name)
+        parser.add_argument('--scenario-name',
+                            required=True,
+                            help='Create installer under scenario name')
+        parser.add_argument('installer',
+                            type=json.loads,
+                            help='Intaller create request format :\n'
+                                 '\'[{"installer": "","versions": []}]\',\n')
+        return parser
+
+    def take_action(self, parsed_args):
+        return self.app.client_manager.post(
+            installers_url(parsed_args.scenario_name), parsed_args.installer)
+
+
+class InstallerDelete(command.Command):
+
+    def get_parser(self, prog_name):
+        parser = super(InstallerDelete, self).get_parser(prog_name)
+        parser.add_argument('--scenario-name',
+                            required=True,
+                            type=str,
+                            help='Delete installer by scenario name')
+        parser.add_argument('name',
+                            nargs='+',
+                            help='Delete installer by name')
+        return parser
+
+    def take_action(self, parsed_args):
+        return self.app.client_manager.delete(
+            installers_url(parsed_args.scenario_name), parsed_args.name)
+
+
+class InstallerPut(command.Command):
+
+    def get_parser(self, prog_name):
+        parser = super(InstallerPut, self).get_parser(prog_name)
+        parser.add_argument('--scenario-name',
+                            type=str,
+                            required=True,
+                            help='Update installer by scenario name')
+        parser.add_argument('installer',
+                            type=json.loads,
+                            help='Intaller create request format :\n'
+                                 '\'[{"installer": "","versions": []}]\',\n')
+        return parser
+
+    def take_action(self, parsed_args):
+        return self.app.client_manager.put(
+            installers_url(
+                parsed_args.scenario_name), parsed_args.installer)
