78cec0a5767652e586721b547031a1ffe4171ed9
thuva4 <tharma.thuva@gmail.com>
2018-04-13 22:42:28 +0530

    Add import support for testapiclient(initial version)
    Change-Id: Ic83bc1ce996ad6de0cd26915c74d601796b6f0d1
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

---
+++ testapi/testapi-client/testapiclient/models/pod.py
@@ -0,0 +1,6 @@
+class Pod():
+    def __init__(self, name, role='', mode='', details=''):
+        self.role = role
+        self.name = name
+        self.details = details
+        self.mode = mode
