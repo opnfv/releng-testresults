693b49ef2933c4bcab66f59b7649ae5102f303de
thuva4 <tharma.thuva@gmail.com>
2018-04-26 13:15:03 +0530

    Add get functionality for the import module
    Module will deserialize the json objects from the server to Pod
    instance.
    
    Change-Id: I3e2cb9386f8949d544624be687ee227ae4529d72
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

--- testapi/testapi-client/testapiclient/models/pods.py
+++ testapi/testapi-client/testapiclient/models/pods.py
@@ -1,6 +1,25 @@
+import json
+import ast
+
+
 class PodCreateRequest(object):
-    def __init__(self, name='', mode='', details='', role=""):
+    def __init__(self, name='', mode='', details='', role=''):
         self.name = name
         self.mode = mode
         self.details = details
         self.role = role
+
+
+class Pod(PodCreateRequest):
+    def __init__(
+        self, _id='', creation_date='', creator='',
+        name='', mode='', details='', role='' ):
+        self._id = _id
+        self.creation_date = creation_date
+        self.creator = creator
+        super(Pod, self).__init__(name, mode, details, role)
+
+    @classmethod
+    def from_json(cls, json_str):
+        json_dict = ast.literal_eval(json.dumps(json_str))
+        return cls(**json_dict)
