4385e43213ce46c258f69d1a5b2cc13ca192fb90
morganrol <morgan.richomme@orange.com>
2021-03-28 23:20:18 +0200

    [TESTAPI] Upgrade the docker without changing the python2.7 code
    migratio to python 3 is not a long flmeuve tranquille
    some libraries used are no more maintained
    this option considers updating the dockerfile without changing the
    original code
    it includes
    - a change of the baseline image (move from 14.04 to 18.04)
    - a better fix of python lib versions
    - the introduction of an ubuntu user (to be better used)
    
    Issue-ID: FUNCTEST-1026
    
    Signed-off-by: morganrol <morgan.richomme@orange.com>
    Change-Id: I37c8262d1ec20151e2402e298751ca39c36d1a7e

--- testapi/docker/prepare-env.sh
+++ testapi/docker/prepare-env.sh
@@ -8,12 +8,12 @@ FILE=/etc/opnfv_testapi/config.ini
 
 auth_server=`echo ${auth:0:1} | tr '[:lower:]' '[:upper:]'``echo ${auth:1} | tr '[:upper:]' '[:lower:]'`
 auth_web=`echo ${auth} | tr '[:upper:]' '[:lower:]'`
-sudo crudini --set --existing ${FILE} mongo url ${mongodb_url}
-sudo crudini --set --existing ${FILE} api url ${base_url}/api/v1
-sudo crudini --set --existing ${FILE} ui url ${base_url}
-sudo crudini --set --existing ${FILE} api authenticate ${auth_server}
+crudini --set --existing ${FILE} mongo url ${mongodb_url}
+crudini --set --existing ${FILE} api url ${base_url}/api/v1
+crudini --set --existing ${FILE} ui url ${base_url}
+crudini --set --existing ${FILE} api authenticate ${auth_server}
 
-sudo cat > /usr/local/share/opnfv_testapi/testapi-ui/config.json << EOF
+cat > /usr/local/share/opnfv_testapi/testapi-ui/config.json << EOF
 {
   "testapiApiUrl": "${base_url}/api/v1",
   "authenticate": ${auth_web}
